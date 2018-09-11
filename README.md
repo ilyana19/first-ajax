269630dfae1cf90ad632b934588c4fcd3b82797d
ilyana19 <aries_2152@hotmail.com>
2018-09-11 12:20:23 -0400

    finished step3456

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/wdi/67/assignments/2055/latest)
+
+1. GET request
+2. ~110ms
+3. 200 OK, it was a success
+4. Other, Document
+5. 200 OK GET Request
+6. XHR, JQuery
--- ajax.js
+++ ajax.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
+  $("#step12 button").click(function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/",
+      method: "GET"
+    });
+  });
 
+  $("#step3456 button").click(function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/ping",
+      method: "GET",
+    }).done(function(responseData) {
+      var response = $("<p>").html(responseData);
+      $("#step3456").append(response);
+    });
+  });
 });
